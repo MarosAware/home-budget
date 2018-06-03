80e55448361bf55622ef536b89841c809cc27f82
MarosAware <marosdevone@gmail.com>
2018-06-03 20:14:00 +0200

    BudgetPosition entity and Controller.

---
+++ src/AppBundle/Controller/BudgetPositionController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BudgetPositionController extends Controller
+{
+
+    /**
+     * @Route("/")
+     * @Method("GET")
+     */
+    public function selectYearGetAction()
+    {
+        $currentDate = new \DateTime();
+        $currentDate = $currentDate->format('Y');
+        return $this->render('@App/BudgetPosition/selectYearForm.html.twig', ['year' => $currentDate]);
+    }
+
+    /**
+     * @Route("/")
+     * @Method("POST")
+     */
+    public function selectYearPostAction(Request $request)
+    {
+        $year = $request->request->get('year');
+        return $this->redirectToRoute('app_budgetposition_showmonths', ['year' => $year]);
+    }
+
+    /**
+     * @Route("/{year}")
+     */
+
+    public function showMonthsAction($year)
+    {
+        return new Response('hello');
+    }
+
+
+}
---
+++ src/AppBundle/Entity/BudgetPosition.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BudgetPosition
+ *
+ * @ORM\Table(name="budget_position")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BudgetPositionRepository")
+ */
+class BudgetPosition
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Category", inversedBy="budgetPosition")
+     */
+    private $category;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return BudgetPosition
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return BudgetPosition
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return BudgetPosition
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return BudgetPosition
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
+
