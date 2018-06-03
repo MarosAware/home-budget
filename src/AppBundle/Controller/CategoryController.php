768c46d064ac0f78e5b965e060d57a97645b378a
Paulina Pietruszewska <paulina.suwalki@gmail.com>
2018-06-04 00:05:30 +0200

    Some changes is Category Controller.

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -11,16 +11,32 @@ use Symfony\Component\HttpFoundation\Request;
 class CategoryController extends Controller
 {
     /**
-     * @Route("/{month}/{year}/details/{category}")
+     * @Route("/{month}/{year}/details/{categoryId}")
      */
-    public function showDetailsAction($month, $category, $year)
+    public function showDetailsAction($month, $categoryId, $year)
     {
+        $category = $this->getDoctrine()->getRepository("AppBundle:Category")->find($categoryId);
+        $categoryName = $category->getName();
+
         $budgetPositions = $this
             ->getDoctrine()
             ->getRepository('AppBundle:BudgetPosition')
-            ->findByMonthAndCategory($month, $year, $category);
+            ->findByMonthAndCategory($month, $year, $categoryId);
 
-        return $this->render('@App/details.html.twig', ['budgetPositions' => $budgetPositions]);
+        $sum = 0;
+
+        foreach ($budgetPositions as $budgetPosition){
+
+            $category = $budgetPosition->getCategory();
+            if ($category->getType() === "przychód"){
+                $sum += $budgetPosition->getPrice();
+
+            }elseif ($category->getType() === "wydatek"){
+                $sum -= $budgetPosition->getPrice();
+            }
+        }
+
+        return $this->render('@App/details.html.twig', ['budgetPositions' => $budgetPositions, 'categoryName' => $categoryName, 'sum' => $sum]);
     }
 
     /**
@@ -46,4 +62,52 @@ class CategoryController extends Controller
         return $this->render('@App/category/add.html.twig', ['form' => $form->createView()]);
     }
 
+    /**
+     * @Route("/modifyCategory/{id}")
+     */
+    public function modifyCategoryAction(Request $request, $id)
+    {
+        $category = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Category')
+            ->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $form = $this->createForm(CategoryType::class, $category);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+//            return $this->redirectToRoute('');
+        }
+
+        return $this->render('@App/category/modify.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/deleteCategory/{id}")
+     */
+    public function deleteCategoryAction(Request $request, $id)
+    {
+        $category = $this
+            ->getDoctrine()
+            ->getRepository('AppBundle:Category')
+            ->find($id);
+
+        if (!$category) {
+            throw $this->createNotFoundException('Category not found');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($category);
+        $em->flush();
+
+        return $this->redirectToRoute('');
+    }
 }
